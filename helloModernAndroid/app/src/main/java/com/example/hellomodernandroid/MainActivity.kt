4d68d874e99f69a80da846e60e692770685cccf1
TaeJuneJoung <jtj0525@gmail.com>
2021-04-05 08:05:21 +0900

    [+]Add livedata

---
+++ helloModernAndroid/app/src/main/java/com/example/hellomodernandroid/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.hellomodernandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.room.Room
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val db = Room.databaseBuilder(
+            applicationContext,
+            AppDatabase::class.java, "database-name"
+        ).allowMainThreadQueries()
+            .build()
+
+        db.todoDao().getAll().observe(this, Observer { todos ->
+            result_text.text = todos.toString()
+        })
+
+        add_btn.setOnClickListener {
+            db.todoDao().insert(Todo(todo_edit.text.toString()))
+
+        }
+    }
+}
